578ea7dd5b26ac2d3326a73167636c3db48dc927
lionsea2020 <78972630+lionsea2020@users.noreply.github.com>
2024-03-03 17:30:52 +0900

    ee

--- lib/view/Firecat/firecat_pollution_bullet.dart
+++ lib/view/Firecat/firecat_pollution_bullet.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
 import 'firecat_pollution_enemy.dart';
 import 'firecat_pollution_main.dart';
 
